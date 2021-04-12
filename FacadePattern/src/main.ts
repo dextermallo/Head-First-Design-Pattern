677e504dc4d712817b2cd71e81a3d338f5c0580d
Dexter Chang <dexter.chang@handshakes.com.sg>
2021-04-12 14:03:35 +0800

    FEAT: init facade pattern

---
+++ FacadePattern/src/main.ts
@@ -0,0 +1,37 @@
+module.exports.main = function() {
+
+}
+
+
+class HomeTheaterFacade {
+    amplifier: Amplifier;
+}
+
+class Amplifier {
+
+}
+
+class Tuner {
+
+}
+
+class DVDPlayer {
+
+}
+
+class CDPlayer {
+
+}
+
+class Projector {
+
+}
+
+class TheaterLights {
+
+}
+
+class Screen {
+
+}
+
